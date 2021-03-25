e8ac44d567a01cdc0630e7c6cfa0a863a134a656
LoanPa <64327463+LoanPa@users.noreply.github.com>
2021-03-25 20:07:44 +0100

    Update

--- main/TDD.py
+++ main/TDD.py
@@ -1,4 +1,23 @@
 
 
-def get_score():
-        pass
+class Game:
+    def __init__(self, player1: str, player2: str):
+        self._player1 = player1
+        self._player2 = player2
+        self._p1Score = 0
+        self._p2Score = 0
+        
+    def player_wins(self, player):
+        if self._player1 == player:
+            self._p1score += 1
+        else:
+            self._p2score += 1  
+        
+    def get_score(self):
+        if _p1Score == _p2Score:
+            return "Fifteen-All"
+        if self._p1Score == 1:
+            return "Fifteen-Love"
+        if self._p1Score == 1:
+            return "Love-Fifteen"
+        return "Love-All"
--- test/test_TDD.py
+++ test/test_TDD.py
@@ -1,13 +1,59 @@
-
 import pytest
+from Game import *
 
 import sys
 sys.path.append('../')
-from main.TDD import *
 
-def test_call_get_score():
-    get_score()
 
-# When game starts return "love-all"
-def test_WhenGameStarts_then_loveAll():
-    get_score()
+@pytest.fixture()
+def setip() -> Game:
+    return Game("Toni", "Carla")
+
+@pytest.fixture()
+def teardown():
+    pass
+
+@pytest.mark.parametrize("score_player_1,score_player_2,expected_result", [
+    (0, 0, "Love-All")
+# (1,1,"Fifteen-All"),
+# ()
+# (14,16,"Win for player2))
+ ])
+def test_whenGameStart_thenLoveAll(setup,  teardown):
+    # ARRANGE
+    game = setup
+    # ACT
+    result = get_score()
+    # ASSERT
+    assert result == "Love-All"
+
+
+def test_whenPlayer1onePoint_thenFifteenLove(setup, teardown):
+    # ARRANGE
+    game = setup
+    # ACT
+    game.player_wins("Toni")
+    result = game.get_score()
+    # ASSERT
+    assert result == "Fifteen-Love"
+
+
+def test_whenPlayer2onePoint_thenFifteenLove(setup, teardown):
+    # ARRANGE
+    game = setup
+    # ACT
+    game.player_wins("Carla")
+    result = game.get_score()
+    # ASSERT
+    assert result == "Love-Fifteen"
+
+
+def test_whenPlayer1onePointandPlayer2onePoint_thenFifteenAll(setup, teardown):
+    # ARRANGE
+    game = setup
+    # ACT
+    game.player_wins("Carla")
+    game.player_wins("Toni")
+    result = game.get_score()
+    # ASSERT
+    assert result == "Fifteen-All"
